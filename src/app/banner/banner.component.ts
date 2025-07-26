8981d25ffda92bd1218ce09aa6c1f8a9395fc91c
MarianaNonato13 <mariana.nonato@tdsinfo.com.br>
2025-07-25 21:26:07 -0300

    ajuste no banner

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent {
+export class BannerComponent implements AfterViewInit {
 
+@ViewChild('bannerVideo') bannerVideo!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit() {
+    const video = this.bannerVideo.nativeElement;
+
+    // Força play e lida com bloqueio de autoplay
+    const playPromise = video.play();
+    if (playPromise !== undefined) {
+      playPromise.catch(err => {
+        console.warn('⚠️ Autoplay bloqueado:', err);
+      });
+    }
+  }
 }
+
